5e36dffdf9bac7557055bcf49865cedf599b0486
Roberto Perez <roberto@debian.oceannet>
2018-06-15 19:21:13 -0700

    Server Routes Part 1 - Event Routes and Controller Complete

--- server/models/event.js
+++ server/models/event.js
@@ -5,14 +5,12 @@ const eventSchema = new Schema({
   eventName: { type: 'String', required: true },
   game: { type: 'String', required: true },
   scheduledDate: { type: 'Date', required: true },
+  slots: { type: 'Number', required: true },
   isFull: { type: "Boolean", default: false },
   slug: { type: 'String', required: true },
   cuid: { type: 'String', required: true },
   dateAdded: { type: 'Date', default: Date.now, required: true },
-  owner: {
-      type: Schema.Types.ObjectId, 
-      ref: "User"
-  }
+  owner: { type: 'String', required: true}
 });
 
 export default mongoose.model('Event', eventSchema);
