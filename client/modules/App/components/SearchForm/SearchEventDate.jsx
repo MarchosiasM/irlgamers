a57a08bc43cff9ec80627feed73506745bbcd49a
Michael Malach <mmalach@ucsd.edu>
2018-07-11 10:25:03 -0700

    Used VSCode to walk back changes in a few files

--- client/modules/App/components/SearchForm/SearchEventDate.jsx
+++ client/modules/App/components/SearchForm/SearchEventDate.jsx
@@ -6,31 +6,20 @@ class DateSearch extends Component {
     this.state = {
       isMounted: false,
     };
-    // this.dateSearch = this.refs.dateSearch;
-    // this.handleDateSearchSubmit = this.handleDateSearchSubmit.bind(this)
   }
 
   componentDidMount() {
     this.setState({ isMounted: true }); // eslint-disable-line
-    // const options = {
-    //     onSelect: function(date){
-    //         console.log(date);
-    //         this.handleDateChange(date);
-    //     },
-    // }; // Options for the Date and TimePicker go here.
-    // Still needs some work in rendering in mobile but works.
     M.AutoInit();
     document.addEventListener('DOMContentLoaded', () => {
       const elem = document.querySelectorAll('.datepicker');
-      const instances = M.Datepicker.init(elem, options);
+      const instances = M.Datepicker.init(elem);
       console.log(instances);
     });
   }
 
   handleDateChange(e) { // This doesn't work because of materialize!
     console.log(e);
-    // console.log(e.target.value);
-    // this.props.dispatch(findEventsByDate(e.target.value));
   }
 
   render() {
