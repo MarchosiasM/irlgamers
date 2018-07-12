5708e7b2d4aa4c71ea4644eaad54503a4a390dea
rperez2021 <rob.perez.flores@gmail.com>
2018-07-11 17:47:12 -0700

    Fixed an Issue with CreateEventWidget. Form was not submitting information into the correct fields in the db. Lesson: the order of the params matter. Also fixed a small issue with missing placeholder text for the time picker.

--- client/modules/Event/pages/EventListPage/EventListPage.js
+++ client/modules/Event/pages/EventListPage/EventListPage.js
@@ -34,10 +34,10 @@ class EventListPage extends Component {
     }
   };
 
-  handleAddEvent = (eventName, game, gameType, address, city, state, zipcode, scheduledDate, scheduledTime, slots, notes, owner) => {
+  handleAddEvent = (eventName, gameType, game, address, city, state, zipcode, scheduledDate, scheduledTime, slots, notes, owner) => {
     this.props.dispatch(toggleAddEvent());
     this.props.dispatch(addEventRequest({
-      eventName, game, gameType, address, city, state, zipcode, scheduledDate, scheduledTime, slots, notes, owner,
+      eventName, gameType, game, address, city, state, zipcode, scheduledDate, scheduledTime, slots, notes, owner
     }));
   };
 
