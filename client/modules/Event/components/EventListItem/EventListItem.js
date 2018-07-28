166088e2c2587908d51ae64ee9b5f6b961e367a7
Janet Hwu <Janet@Janets-Laptop.local>
2018-07-28 14:14:53 -0700

    event cards

--- client/modules/Event/components/EventListItem/EventListItem.js
+++ client/modules/Event/components/EventListItem/EventListItem.js
@@ -10,46 +10,34 @@ function EventListItem(props) {
     <div>
       
       <div className="row">
-        <div className="col s12 m7">
+        <div className="col s12 m6">
           <div className="card">
             <div className="card-image">
               <img src="http://atomicgametheory.com/wp-content/uploads/catan.jpg" />
-              <span className={`card-title`}>
-                {props.event.eventName}
+              <span className={`card-title ${styles['card-title-banner']}`}>
+                <span className={styles['event-name']}>{props.event.eventName}</span>
+                <span className={styles['game-name']}><em>{props.event.game}</em></span>
               </span>
             </div>
             <div className="card-content">
-              <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
+              <div className={styles['author-name']}>
+              {' Created by: '}
+                <Link className={styles['link']} to={`/profile/${props.event.owner}`}>
+                  {props.event.ownerName}
+                </Link>
+              </div>
+              <div className={styles['post-desc']}>{moment(props.event.scheduledDate).format('MMM Do YYYY')}  |   {props.event.scheduledTime}</div>
+              <div className={styles['post-desc']}>playing <em>{props.event.game}</em></div>
+              <div className={styles['post-desc']}><span className={styles['attendee-count']}>{props.event.attendees.length}</span> of <span className={styles['seat-count']}>{props.event.slots}</span> seats filled</div>
+              <div className={styles['post-desc']}>{props.event.notes}</div>
             </div>
             <div className="card-action">
-              <Link to={`/games/${props.event.slug}-${props.event.cuid}`} >
+              <Link className={styles['link2']} to={`/games/${props.event.slug}-${props.event.cuid}`} >
                 view this event
               </Link>
             </div>
           </div>
         </div>
-      </div>
-
-
-      <div className={styles['single-post']}>
-        <h3 className={styles['post-title']}>
-          <Link to={`/games/${props.event.slug}-${props.event.cuid}`} >
-            {props.event.eventName}
-          </Link>
-        </h3>
-        <p className={styles['author-name']}>
-        {' by '}
-          <Link to={`/profile/${props.event.owner}`}>
-            {props.event.ownerName}
-          </Link>
-        </p>
-        <p className={styles['post-desc']}>{props.event.game}</p>
-        <p className={styles['post-desc']}>{moment(props.event.scheduledDate).format('MMM Do YYYY')}</p>
-        <p className={styles['post-desc']}>{props.event.scheduledTime}</p>
-        <p className={styles['post-desc']}>{props.event.attendees.length}/{props.event.slots}</p>
-        <p className={styles['post-desc']}>{props.event.notes}</p>
-        <hr className={styles.divider} />
       </div>
 
     </div>
