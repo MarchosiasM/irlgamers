9d7e0ec97f981e866683d8d4490c3e7f01c77b79
Janet Hwu <Janet@Janets-Laptop.local>
2018-07-28 19:29:44 -0700

    card image randomization

--- client/modules/Event/components/EventListItem/stockImages.js
+++ client/modules/Event/components/EventListItem/stockImages.js
@@ -1,103 +1,103 @@
 const images = [
-    { 
-        game: 'Catan', 
-        type: 'Board Games', 
-        image: 'http://atomicgametheory.com/wp-content/uploads/catan.jpg' 
-    },
-    { 
-        game: 'Tzolkin', 
-        type: 'Board Games', 
-        image: 'https://i.pinimg.com/originals/e7/1b/42/e71b42729d26643c01fc4500da26c2ef.jpg' 
-    },
-    { 
-        game: 'Yahtzee', 
-        type: 'Dice Games', 
-        image: 'https://geekandsundry.com/wp-content/uploads/2017/04/Roll-and-Write-FI-970x545.png' 
-    },
-    { 
-        game: 'Texas Holdem', 
-        type: 'Card Games', 
-        image: 'https://static.boydgaming.net/kansasstar/assets/hero/KS_TexasHoldEm_MobileHero640x356.jpg' 
-    },
-    { 
-        game: 'The Resistance', 
-        type: 'Information Games', 
-        image: 'http://mcmgaming.com/wp-content/uploads/2017/04/The-Resistance-Game.jpg' 
-    },
-    { 
-        game: 'Bohnanza', 
-        type: 'Card Games', 
-        image: 'http://www.durangoescaperooms.co/uploads/8/0/0/3/80036362/s820677207579784711_p115_i3_w1374.jpeg' 
-    },
-    { 
-        game: 'Codenames', 
-        type: 'Card Games', 
-        image: 'http://www.boardgameauthority.com/wp-content/uploads/2016/11/codenames-board-game.jpg' 
-    },
-    { 
-        game: 'Shadow Hunters', 
-        type: 'Board Games', 
-        image: 'https://i.ytimg.com/vi/8ryGDB68_as/maxresdefault.jpg' 
-    },
-    { 
-        game: 'King\'s Cup', 
-        type: 'Drinking Games', 
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Kings_Cup.png/1200px-Kings_Cup.png' 
-    },
-    { 
-        game: 'Battleship', 
-        type: 'Pen and Paper', 
-        image: 'https://thumbs.dreamstime.com/b/battleship-game-28048799.jpg' 
-    },
-    { 
-        game: 'Dungeons & Dragons', 
-        type: 'Role-playing Games', 
-        image: 'https://cdn.vox-cdn.com/thumbor/wrG0KWYx-T4Ki0o6oqn02jY8eYA=/0x0:776x517/1200x675/filters:focal(387x354:511x478)/cdn.vox-cdn.com/uploads/chorus_image/image/59853017/Screen_Shot_2015-01-13_at_3.16.45_PM.0.0.png' 
-    },
-    { 
-        game: 'Warhammer', 
-        type: 'Strategy Games', 
-        image: 'https://i.ytimg.com/vi/IhZTMs4FyZs/maxresdefault.jpg' 
-    },
-    { 
-        game: 'Fortnite', 
-        type: 'Video Games', 
-        image: 'http://digitalspyuk.cdnds.net/18/15/980x490/landscape-1523636434-capture52.PNG' 
-    },
-    { 
-        game: 'Cornhole', 
-        type: 'Outdoor', 
-        image: 'http://cdn.shopify.com/s/files/1/1395/8847/products/hc-cornhole-striped4-sq_grande.jpg?v=1524675567' 
-    },
-    { 
-        game: 'Mahjong', 
-        type: 'Tile-based Games', 
-        image: 'https://i1.wp.com/www.thedailychina.org/wp-content/uploads/2018/03/Screen-Shot-2018-03-09-at-6.39.33-PM.png' 
-    },
-    { 
+    { // 0
         game: 'Pathfinder Adventures', 
         type: 'Adventure Games', 
         image: 'https://venturebeat.com/wp-content/uploads/2017/05/pa-goblin-raider-merisiel.png?fit=578%2C361&strip=all' 
     },
-    { 
-        game: 'PUBG', 
-        type: 'Video Games', 
-        image: 'https://icdn2.digitaltrends.com/image/playerunkowns-battlegrounds-pubg-gameplay-screenshot.png' 
+    { // 1
+        game: 'Catan', 
+        type: 'Board Games', 
+        image: 'http://atomicgametheory.com/wp-content/uploads/catan.jpg' 
     },
-    { 
-        game: 'Chess', 
-        type: 'Strategy Games', 
-        image: 'https://images.chesscomfiles.com/uploads/v1/article/17623.87bb05cd.630x354o.cb5899315c4c.jpeg' 
+    { // 2
+        game: 'Tzolkin', 
+        type: 'Board Games', 
+        image: 'https://i.pinimg.com/originals/e7/1b/42/e71b42729d26643c01fc4500da26c2ef.jpg' 
     },
-    { 
+    { // 3
+        game: 'Shadow Hunters', 
+        type: 'Board Games', 
+        image: 'https://i.ytimg.com/vi/8ryGDB68_as/maxresdefault.jpg' 
+    },
+    { // 4
+        game: 'Texas Holdem', 
+        type: 'Card Games', 
+        image: 'https://static.boydgaming.net/kansasstar/assets/hero/KS_TexasHoldEm_MobileHero640x356.jpg' 
+    },
+    { // 5
+        game: 'Bohnanza', 
+        type: 'Card Games', 
+        image: 'http://www.durangoescaperooms.co/uploads/8/0/0/3/80036362/s820677207579784711_p115_i3_w1374.jpeg' 
+    },
+    { // 6
+        game: 'Codenames', 
+        type: 'Card Games', 
+        image: 'http://www.boardgameauthority.com/wp-content/uploads/2016/11/codenames-board-game.jpg' 
+    },
+    { // 7
+        game: 'Yahtzee', 
+        type: 'Dice Games', 
+        image: 'https://geekandsundry.com/wp-content/uploads/2017/04/Roll-and-Write-FI-970x545.png' 
+    },
+    { // 8
+        game: 'King\'s Cup', 
+        type: 'Drinking Games', 
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Kings_Cup.png/1200px-Kings_Cup.png' 
+    },
+    { // 9
+        game: 'The Resistance', 
+        type: 'Information Games', 
+        image: 'http://mcmgaming.com/wp-content/uploads/2017/04/The-Resistance-Game.jpg' 
+    }, 
+    { // 10
+        game: 'Cornhole', 
+        type: 'Outdoor Games', 
+        image: 'http://cdn.shopify.com/s/files/1/1395/8847/products/hc-cornhole-striped4-sq_grande.jpg?v=1524675567' 
+    },
+    { // 11
+        game: 'Taboo', 
+        type: 'Party Games', 
+        image: 'https://8hl294baqgehxrq9ps4y7x3-wpengine.netdna-ssl.com/wp-content/uploads/2015/04/game-in-play-2-wm.jpg' 
+    },
+    { // 12
+        game: 'Battleship', 
+        type: 'Pen and Paper', 
+        image: 'https://thumbs.dreamstime.com/b/battleship-game-28048799.jpg' 
+    },
+    { // 13
         game: 'Scattergories', 
         type: 'Pen and Paper', 
         image: 'https://www.thedatingdivas.com/wp-content/uploads/SpouseScattergories3-Web-Size.jpg' 
     },
-    { 
-        game: 'Taboo', 
-        type: 'Party Games', 
-        image: 'https://8hl294baqgehxrq9ps4y7x3-wpengine.netdna-ssl.com/wp-content/uploads/2015/04/game-in-play-2-wm.jpg' 
+    { // 14
+        game: 'Dungeons & Dragons', 
+        type: 'Role-playing Games', 
+        image: 'https://cdn.vox-cdn.com/thumbor/wrG0KWYx-T4Ki0o6oqn02jY8eYA=/0x0:776x517/1200x675/filters:focal(387x354:511x478)/cdn.vox-cdn.com/uploads/chorus_image/image/59853017/Screen_Shot_2015-01-13_at_3.16.45_PM.0.0.png' 
+    },
+    { // 15
+        game: 'Chess', 
+        type: 'Strategy Games', 
+        image: 'https://images.chesscomfiles.com/uploads/v1/article/17623.87bb05cd.630x354o.cb5899315c4c.jpeg' 
+    },
+    { // 16
+        game: 'Warhammer', 
+        type: 'Strategy Games', 
+        image: 'https://i.ytimg.com/vi/IhZTMs4FyZs/maxresdefault.jpg' 
+    },
+    { // 17
+        game: 'Mahjong', 
+        type: 'Tile-based Games', 
+        image: 'https://i1.wp.com/www.thedailychina.org/wp-content/uploads/2018/03/Screen-Shot-2018-03-09-at-6.39.33-PM.png' 
+    },
+    { // 18
+        game: 'Fortnite', 
+        type: 'Video Games', 
+        image: 'http://digitalspyuk.cdnds.net/18/15/980x490/landscape-1523636434-capture52.PNG' 
+    },
+    { // 19
+        game: 'PUBG', 
+        type: 'Video Games', 
+        image: 'https://icdn2.digitaltrends.com/image/playerunkowns-battlegrounds-pubg-gameplay-screenshot.png' 
     }
 ]
 
